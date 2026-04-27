5f421cb0c05fdf65457b6b68b26335f934eca44c
Lena Voytek <lena@voytek.dev>
2026-04-27 15:38:21 -0400

    Export bug models

--- src/ubq/models/__init__.py
+++ src/ubq/models/__init__.py
@@ -1,8 +1,11 @@
 """Generic Ubuntu data models."""
 
 from ubq.models.common import CommentRecord, UserRecord
+from ubq.models.bug import BugRecord, BugTaskRecord
 
 __all__ = [
     "CommentRecord",
     "UserRecord",
+    "BugRecord",
+    "BugTaskRecord",
 ]
