8e9d468054a296b50a641624876fd1cdfd51b89c
Lena Voytek <lena@voytek.dev>
2026-04-28 15:06:53 -0400

    Add a base Launchpad package provider

---
+++ src/ubq/providers/launchpad/package.py
@@ -0,0 +1,13 @@
+"""Launchpad package data provider."""
+
+from ubq.models import PackageRecord
+from ubq.providers.launchpad.provider import LaunchpadProvider
+from ubq.providers.package import PackageProvider
+
+
+class LaunchpadPackageProvider(LaunchpadProvider, PackageProvider):
+    """Provider implementation for Launchpad packages."""
+
+    def get_package(self, package_name: str) -> PackageRecord:
+        """Fetch a Launchpad package by name."""
+        ...
