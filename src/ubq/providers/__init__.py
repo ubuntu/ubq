e19fb0b7bf5f41e55b2cf618e3921e777a579408
Lena Voytek <lena@voytek.dev>
2026-04-28 15:07:38 -0400

    Update provider exports

--- src/ubq/providers/__init__.py
+++ src/ubq/providers/__init__.py
@@ -2,16 +2,20 @@
 
 from ubq.providers.bug import BugProvider
 from ubq.providers.launchpad.bug import LaunchpadBugProvider
+from ubq.providers.launchpad.package import LaunchpadPackageProvider
 from ubq.providers.merge_request import MergeRequestProvider
 from ubq.providers.package import PackageProvider
 from ubq.providers.provider import Provider
+from ubq.providers.session import ProviderSession
 from ubq.providers.version import VersionProvider
 
 __all__ = [
     "Provider",
+    "ProviderSession",
     "BugProvider",
     "PackageProvider",
     "VersionProvider",
     "MergeRequestProvider",
     "LaunchpadBugProvider",
+    "LaunchpadPackageProvider",
 ]
--- src/ubq/providers/launchpad/__init__.py
+++ src/ubq/providers/launchpad/__init__.py
@@ -1,7 +1,11 @@
 """Launchpad data provider implementations."""
 
 from ubq.providers.launchpad.bug import LaunchpadBugProvider
+from ubq.providers.launchpad.package import LaunchpadPackageProvider
+from ubq.providers.launchpad.provider import LaunchpadProvider
 
 __all__ = [
+    "LaunchpadProvider",
     "LaunchpadBugProvider",
+    "LaunchpadPackageProvider",
 ]
