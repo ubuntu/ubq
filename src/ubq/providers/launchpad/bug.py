e2c0d20f6d978004405ce73ab41f904cacc80e9f
Lena Voytek <lena@voytek.dev>
2026-04-28 15:03:06 -0400

    Update LaunchpadBug to use common LP Provider

--- src/ubq/providers/launchpad/bug.py
+++ src/ubq/providers/launchpad/bug.py
@@ -2,12 +2,11 @@
 
 from ubq.models import BugRecord
 from ubq.providers.bug import BugProvider
+from ubq.providers.launchpad.provider import LaunchpadProvider
 
 
-class LaunchpadBugProvider(BugProvider):
+class LaunchpadBugProvider(LaunchpadProvider, BugProvider):
     """Provider implementation for Launchpad bugs."""
-
-    provider_name = "launchpad"
 
     def get_bug(self, bug_id: str) -> BugRecord:
         """Fetch a Launchpad bug by identifier."""
