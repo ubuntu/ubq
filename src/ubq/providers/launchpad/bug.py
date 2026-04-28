5dcab30f64f9fb0254e731d42f971de2e48a01cd
Lena Voytek <lena@voytek.dev>
2026-04-28 16:51:56 -0400

    Begin lp get bug implementation

--- src/ubq/providers/launchpad/bug.py
+++ src/ubq/providers/launchpad/bug.py
@@ -1,8 +1,12 @@
 """Launchpad bug data provider."""
 
-from ubq.models import BugRecord
+from lazr.restfulclient.resource import Entry
+
+from ubq.models import BugRecord, UserRecord
 from ubq.providers.bug import BugProvider
 from ubq.providers.launchpad.provider import LaunchpadProvider
+
+BASE_BUG_URL = "https://api.launchpad.net/devel/ubuntu/+bug/"
 
 
 class LaunchpadBugProvider(LaunchpadProvider, BugProvider):
@@ -10,3 +14,21 @@ class LaunchpadBugProvider(LaunchpadProvider, BugProvider):
 
     def get_bug(self, bug_id: str) -> BugRecord:
         """Fetch a Launchpad bug by identifier."""
+        if self._launchpad is None:
+            raise RuntimeError("Launchpad not yet authenticated. Run 'authenticate()' first.")
+
+        lp_object = self._launchpad.load(BASE_BUG_URL + bug_id)
+
+        # Launchpad will sometimes return default bug_task for a bug, if so it needs to be
+        # converted to a bug explicitly. Otherwise assume it's already a bug.
+        try:
+            lp_bug = lp_object.bug
+        except AttributeError:
+            lp_bug = lp_object
+
+        return BugRecord(
+            provider_name=self.provider_name,
+            id=str(lp_bug.id),
+            title=lp_bug.title,
+            created_at=lp_bug.date_created,
+        )
